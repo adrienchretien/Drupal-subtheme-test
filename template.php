e5717dbbb3a434c0dbdabb66f109078bfc866ae2
Neol <adrien.chre@gmail.com>
2015-07-09 10:04:26 +0200

    Add hook to exclude css files defined in info file

--- template.php
+++ template.php
@@ -1,33 +1,26 @@
 <?php
-
 /**
  * @file
  * template.php
  */
 
 /**
- * Implements hook_preprocess_html().
+ * Include common functions used through out theme.
  */
-function bootstrap_subtheme_preprocess_html(&$vars) {
-  if ($node = menu_get_object()) {
-    $node_wrapper = entity_metadata_wrapper('node', $node);
+include_once dirname(__FILE__) . '/theme/common.inc';
 
-    //If the content type of this node has a field, field_body_classes_select,
-    //let's pass the value(s) of the field to the body class in html.tpl.php
-    if (isset($node_wrapper->field_body_classes_select)){
-      $body_classes = $node_wrapper->field_body_classes_select->value();
-      foreach ($body_classes as $body_class) {
-	$vars['classes_array'][] = $body_class;
-      }
-    }
-
-    //If the content type of this node has a field, field_custom_body_classes,
-    //let's pass the value(s) of the field to the body class in html.tpl.php
-    if (isset($node_wrapper->field_custom_body_classes)){
-      $custom_body_classes = $node_wrapper->field_custom_body_classes->value();
-      foreach ($custom_body_classes as $custom_body_class) {
-	$vars['classes_array'][] = $custom_body_class;
-      }
-    }
-  }
+/**
+ * Implements hook_theme().
+ */
+function theme_minima_theme(&$existing, $type, $theme, $path) {
+  theme_minima_include($theme, 'theme/registry.inc');
+  return _theme_minima_theme($existing, $type, $theme, $path);
 }
+
+/**
+ * Declare various hook_*_alter() hooks.
+ *
+ * hook_*_alter() implementations must live (via include) inside this file so
+ * they are properly detected when drupal_alter() is invoked.
+ */
+theme_minima_include('theme_minima', 'theme/alter.inc');
